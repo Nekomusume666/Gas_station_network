61af9410077ad180616ab5f41b84d89e52a816c7
bobkov.maxim2018@yandex.ru <bobkov.maxim2018@yandex.ru>
2024-04-30 20:27:10 +0300

    Добавил функцию добавления пользователя

---
+++ Gas_station_network/DataBase/Storage/IPersonStorage.cs
@@ -0,0 +1,8 @@
+﻿using Gas_station_network.Model;
+
+namespace Gas_station_network.DataBase.Storage;
+
+interface IPersonStorage
+{
+    public void AddPerson(Person person);
+}
---
+++ Gas_station_network/DataBase/Storage/PersonStorage.cs
@@ -0,0 +1,18 @@
+﻿using Gas_station_network.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gas_station_network.DataBase.Storage;
+
+class PersonStorage : IPersonStorage
+{
+    public void AddPerson(Person person)
+    {
+        using (var context = new PersonDbContext())
+        {
+            var entity = context.Add(person);
+            entity.State = EntityState.Added;
+
+            context.SaveChanges();
+        }
+    }
+}
