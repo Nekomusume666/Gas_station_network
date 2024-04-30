529d49e08d10a77ab798d8a11c45860901a301ad
bobkov.maxim2018@yandex.ru <bobkov.maxim2018@yandex.ru>
2024-04-30 19:35:20 +0300

    Добавил модели

---
+++ Gas_station_network/Model/GasStation.cs
@@ -0,0 +1,39 @@
+﻿namespace Gas_station_network.Model;
+
+/// <summary>
+/// Класс, характеризующий АЗС.
+/// </summary>
+class GasStation
+{
+    /// <summary>
+    /// Адрес.
+    /// </summary>
+    public string Address { get; set; }
+
+    /// <summary>
+    /// Колонки.
+    /// </summary>
+    public List<Column> Columns { get; set; }
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="address">Адрес.</param>
+    public GasStation(string address)
+    {
+        Address = address;
+        Columns = new List<Column>();
+
+        // заглушка (пока так :) )
+        AddColumn(1, true, "A95", 100);
+        AddColumn(1, true, "A98", 100);
+        AddColumn(1, true, "A100", 100);
+    }
+
+    public void AddColumn(int number, bool isActive, string name, int volume)
+    {
+        Column column = new Column(number, isActive, name, volume);
+
+        Columns.Add(column);
+    }
+}
