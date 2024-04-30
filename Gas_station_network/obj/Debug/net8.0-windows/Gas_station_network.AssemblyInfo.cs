ed9f0625998c075a7b9d89ff4a3651858d5c63da
Nekomusume666 <ivishnikin@inbox.ru>
2024-04-30 18:26:07 +0300

    add empty folders

--- Gas_station_network/obj/Debug/net8.0-windows/Gas_station_network.AssemblyInfo.cs
+++ Gas_station_network/obj/Debug/net8.0-windows/Gas_station_network.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gas_station_network")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f4ae861fd06b9e6a68917e978ce397f20e8b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gas_station_network")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gas_station_network")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
