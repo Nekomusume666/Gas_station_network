60f4ae861fd06b9e6a68917e978ce397f20e8b28
Nekomusume666 <ivishnikin@inbox.ru>
2024-04-30 18:18:35 +0300

    init commit

---
+++ Gas_station_network/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Gas_station_network
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
