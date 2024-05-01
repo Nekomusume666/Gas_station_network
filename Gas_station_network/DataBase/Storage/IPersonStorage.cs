39aef4b84a038f4890e49372cf271936d6c346d6
bobkov.maxim2018@yandex.ru <bobkov.maxim2018@yandex.ru>
2024-05-01 10:59:12 +0300

    Добавил функции для входа и регистрации.

--- Gas_station_network/DataBase/Storage/IPersonStorage.cs
+++ Gas_station_network/DataBase/Storage/IPersonStorage.cs
@@ -2,7 +2,29 @@
 
 namespace Gas_station_network.DataBase.Storage;
 
+/// <summary>
+/// Интерфейс для взаимодействия с БД пользователей.
+/// </summary>
 interface IPersonStorage
 {
+    /// <summary>
+    /// Добавить пользователя.
+    /// </summary>
+    /// <param name="person">Пользователь.</param>
     public void AddPerson(Person person);
+
+    /// <summary>
+    /// Проверка существования пользователя.
+    /// </summary>
+    /// <param name="login">Логин.</param>
+    /// <param name="password">Пароль.</param>
+    /// <returns>Результат проверки.</returns>
+    public bool CheckPerson(string login, string password);
+
+    /// <summary>
+    /// Проверка логина на уникальность.
+    /// </summary>
+    /// <param name="login">Логин.</param>
+    /// <returns>Результат проверки.</returns>
+    public bool CheckUniqueLogin(string login);
 }
--- Gas_station_network/DataBase/Storage/PersonStorage.cs
+++ Gas_station_network/DataBase/Storage/PersonStorage.cs
@@ -3,8 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Gas_station_network.DataBase.Storage;
 
+/// <summary>
+/// Объект для взаимодействия с БД пользователей.
+/// </summary>
 class PersonStorage : IPersonStorage
 {
+    /// <summary>
+    /// Добавить пользователя.
+    /// </summary>
+    /// <param name="person">Пользователь.</param>
     public void AddPerson(Person person)
     {
         using (var context = new PersonDbContext())
@@ -13,6 +20,33 @@ class PersonStorage : IPersonStorage
             entity.State = EntityState.Added;
 
             context.SaveChanges();
+        }
+    }
+
+    /// <summary>
+    /// Проверка существования пользователя.
+    /// </summary>
+    /// <param name="login">Логин.</param>
+    /// <param name="password">Пароль.</param>
+    /// <returns>Результат проверки.</returns>
+    public bool CheckPerson(string login, string password)
+    {
+        using (var context = new PersonDbContext())
+        {
+            return context.Persons.Any(p => p.Login == login && p.Password == password);
+        }
+    }
+
+    /// <summary>
+    /// Проверка логина на уникальность.
+    /// </summary>
+    /// <param name="login">Логин.</param>
+    /// <returns>Результат проверки.</returns>
+    public bool CheckUniqueLogin(string login)
+    {
+        using (var context = new PersonDbContext())
+        {
+            return !context.Persons.Any(p => p.Login == login);
         }
     }
 }
