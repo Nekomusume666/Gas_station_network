789b5cee8d7fef77ae0b06f38dad813393c61fb0
bobkov.maxim2018@yandex.ru <bobkov.maxim2018@yandex.ru>
2024-05-01 16:17:59 +0300

    Добавлен функционал

---
+++ Gas_station_network/DataBase/Storage/Technic/ITechnicStorage.cs
@@ -0,0 +1,21 @@
+﻿using Gas_station_network.Model;
+
+namespace Gas_station_network.DataBase.Storage.Technic;
+
+/// <summary>
+/// Интерфейс, представляющий функционал техника.
+/// </summary>
+interface ITechnicStorage
+{
+    /// <summary>
+    /// Выключение колонки.
+    /// </summary>
+    /// <param name="column">Колонка.</param>
+    public void OffColumn(Column column);
+
+    /// <summary>
+    /// Включение колонки.
+    /// </summary>
+    /// <param name="column">Колонка.</param>
+    public void OnColumn(Column column);
+}
---
+++ Gas_station_network/DataBase/Storage/Technic/TechnicStorage.cs
@@ -0,0 +1,28 @@
+﻿using Gas_station_network.Core;
+using Gas_station_network.Model;
+
+namespace Gas_station_network.DataBase.Storage.Technic;
+
+/// <summary>
+/// Класс, представляющий функционал техника.
+/// </summary>
+internal class TechnicStorage : ITechnicStorage
+{
+    public void OffColumn(Column column)
+    {
+        using (var context = new GasStationDbContext())
+        {
+            column.IsActive = false;
+            context.SaveChanges();
+        }
+    }
+
+    public void OnColumn(Column column)
+    {
+        using (var context = new GasStationDbContext())
+        {
+            column.IsActive = true;
+            context.SaveChanges();
+        }
+    }
+}
